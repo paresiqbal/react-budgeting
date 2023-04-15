c96e1704f1505bb6a53eaaf0a707d5eb25d8df02
paresiqbal <thecrowdel@gmail.com>
2023-04-15 21:22:41 +0700

    Adding error pages

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,3 +1,13 @@
+import { useRouteError } from "react-router";
+
 export default function Error() {
-  return <div>404 Error</div>;
+  const error = useRouteError();
+
+  return (
+    <div className="error">
+      <h1>Uh oh! We've got a problem. </h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md"></div>
+    </div>
+  );
 }
