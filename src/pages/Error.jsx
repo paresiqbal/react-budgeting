e7242df312fdd7aafbc15fd0a3f5b3f9933ba159
paresiqbal <thecrowdel@gmail.com>
2023-04-15 21:27:59 +0700

    Adding error and navigate

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,27 @@
-import { useRouteError } from "react-router";
+import { useRouteError, Link, useNavigate } from "react-router-dom";
+
+// library
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 
 export default function Error() {
   const error = useRouteError();
+
+  const navigate = useNavigate();
 
   return (
     <div className="error">
       <h1>Uh oh! We've got a problem. </h1>
       <p>{error.message || error.statusText}</p>
-      <div className="flex-md"></div>
+      <div className="flex-md">
+        <button className="btn btn--dark" onClick={() => navigate(-1)}>
+          <ArrowUturnLeftIcon width={20} />
+          <span>Go Back</span>
+        </button>
+        <Link to="/" className="btn btn--dark">
+          <HomeIcon width={20} />
+          <span>Go Home</span>
+        </Link>
+      </div>
     </div>
   );
 }
